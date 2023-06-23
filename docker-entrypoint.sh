e4c3132b2433cb9ae14225a89ab4db3f8d986d69
apogiatzis <apoyiatzis@outlook.com>
2023-06-23 01:53:33 +0100

    fix: permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -25,8 +25,6 @@ python ping.py
 python manage.py db upgrade
 
 # [OPTIONAL] Login to github container registry - for downloading private images
-sudo chown -R ctfd:ctfd /home/ctfd
-sudo chmod -R 644 ctfd:ctfd /home/ctfd
 echo ${CONTAINER_REGISTRY_PAT} | docker login ghcr.io -u ${CONTAINER_REGISTRY_USERNAME} --password-stdin
 
 # Start CTFd
